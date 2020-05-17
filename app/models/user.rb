8390a4fe4c547f217bc6b139dd641288783e9070
hokudai <ken4600223@gmail.com>
2020-05-17 11:45:49 +0900

    sendgrid email 試し

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, #:confirmable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
   #validate :check_email
   has_many :cards
