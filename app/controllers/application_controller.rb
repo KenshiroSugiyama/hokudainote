f9b5e4ffa9334410d54d327c03d39e0dabb49eef
hokudai <ken4600223@gmail.com>
2020-07-03 05:35:07 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+
+  def after_sign_in_path_for(resource)
+     notes_path# ログイン後に遷移するpathを設定
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :accepted)}
   end
